8b8faf1f2f945ab6dbb0a4a2ef2ad6e7bb23cc94
Egor <148006251+EgorGoluzin@users.noreply.github.com>
2023-12-11 22:53:28 +0300

    Create subj.h

---
+++ lab3/subj.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Subject
+{
+public:
+    int f1(int a, int b)
+    {
+        return a + b;
+    };
+
+    int f2(int a)
+    {
+        return a * 2;
+    };
+
+    int f3(int a, int b)
+    {
+        return a * b;
+    };
+};
